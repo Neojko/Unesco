78211b29e5c4e60983d97fbadde494d8095eefeb
Julien <julien.fradin@tesco.com>
2022-07-04 23:08:36 +0100

    Julien | LNS | Added Algorithm and LNS -- needs tests

---
+++ src/main/java/optimisation/algorithms/LNS.java
@@ -0,0 +1,51 @@
+package optimisation.algorithms;
+
+import domain.constraints.ConstraintManager;
+import domain.matrix.TravelMatrix;
+import domain.objectives.ObjectiveManager;
+import domain.solution.Solution;
+import lombok.Builder;
+import lombok.var;
+import optimisation.criteria.acceptance.SolutionAcceptanceCriterion;
+import optimisation.criteria.stopping.StoppingCriterion;
+import optimisation.modificators.destroyers.Destroyer;
+import optimisation.modificators.repairers.Repairer;
+
+/**
+ * LNS stands for Large Neighborhood Search. It's a metaheuristic that iteratively destroy and
+ * repair a solution. At the end of each iteration, we can use a solution acceptance criterion to
+ * determine which solution should the next current solution be.
+ */
+
+@Builder
+public class LNS implements Algorithm {
+
+  private final Destroyer destroyer;
+  private final Repairer repairer;
+  private final SolutionAcceptanceCriterion acceptanceCriterion;
+  private final StoppingCriterion stoppingCriterion;
+
+
+  @Override
+  public Solution improve(
+      final ConstraintManager constraintManager,
+      final ObjectiveManager objectiveManager,
+      final TravelMatrix matrix,
+      final Solution solution) {
+    var bestSolution = solution.copy();
+    var currentSolution = solution.copy();
+    stoppingCriterion.initialise();
+    while (!stoppingCriterion.isMet()) {
+      destroyer.destroy(objectiveManager, matrix, currentSolution);
+      repairer.repair(constraintManager, objectiveManager, matrix, currentSolution);
+      if (currentSolution.isBetterThan(bestSolution)) {
+        bestSolution = currentSolution.copy();
+      }
+      else if (!acceptanceCriterion.accept(currentSolution, bestSolution)) {
+        currentSolution = bestSolution.copy();
+      }
+      stoppingCriterion.update();
+    }
+    return bestSolution;
+  }
+}
---
+++ src/test/java/optimisation/algorithms/LNSTest.java
@@ -0,0 +1,7 @@
+package optimisation.algorithms;
+
+public class LNSTest {
+
+  
+
+}
