8fab580542d2fe4a657f282c00b4654bc02a592f
Julien <julien.fradin@tesco.com>
2022-07-04 21:58:39 +0100

    Julien | VisitNewSiteRepairer | Before mvn

--- src/test/java/optimisation/moves/UnvisitSiteMoveTest.java
+++ src/test/java/optimisation/moves/UnvisitSiteMoveTest.java
@@ -1,10 +1,10 @@
 package optimisation.moves;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import domain.constraints.ConstraintManager;
 import domain.locations.sites.Site;
 import domain.matrix.TravelMatrix;
 import domain.objectives.ObjectiveManager;
@@ -17,50 +17,42 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-public class VisitNewSiteMoveTest {
+public class UnvisitSiteMoveTest {
 
-  private boolean isFeasible;
   private ObjectiveValues objectiveValues;
-  private VisitNewSiteMove move;
+  private UnvisitSiteMove move;
 
   @BeforeEach
   public void setUp() {
     final var solution = mock(Solution.class);
     final var site = mock(Site.class);
-    final var position = 2;
     final TravelMatrix matrix = mock(TravelMatrix.class);
-    isFeasible = true;
     objectiveValues = mock(ObjectiveValues.class);
-    final ConstraintManager constraintManager = mock(ConstraintManager.class);
     final long tripDurationDelta = 1L;
     try (MockedStatic<SolutionTripDurationComputer> mockDelta =
         Mockito.mockStatic(SolutionTripDurationComputer.class)) {
       mockDelta
           .when(
               () ->
-                  SolutionTripDurationComputer.computeTripDurationDeltaToVisitNewSite(
-                      solution, site, position, matrix))
+                  SolutionTripDurationComputer.computeTripDurationDeltaToUnvisitSite(
+                      solution, site, matrix))
           .thenReturn(tripDurationDelta);
-      when(constraintManager.canVisitNewSite(solution, site, position, tripDurationDelta))
-          .thenReturn(isFeasible);
       final ObjectiveManager objectiveManager = mock(ObjectiveManager.class);
-      when(objectiveManager.computeVisitNewSiteObjectiveValuesDelta(
-              solution, site, tripDurationDelta))
+      when(objectiveManager.computeUnvisitSiteObjectiveValuesDelta(
+          solution, site, tripDurationDelta))
           .thenReturn(objectiveValues);
-      move =
-          new VisitNewSiteMove(
-              solution, site, position, matrix, constraintManager, objectiveManager);
+      move = new UnvisitSiteMove(solution, site, matrix, objectiveManager);
     }
   }
 
   @Test
   public void test_is_feasible() {
-
-    assertEquals(isFeasible, move.isFeasible());
+    assertTrue(move.isFeasible());
   }
 
   @Test
   public void test_get_objective_values_delta() {
     assertEquals(objectiveValues, move.getObjectiveValuesDelta());
   }
+
 }
